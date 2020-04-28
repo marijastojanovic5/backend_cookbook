cf728ef9d62408123d3dd56dbba0909175d8c47a
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-04-28 11:02:36 -0400

    recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,12 @@
 class RecipesController < ApplicationController
+
+    def index
+        recipe =Recipe.all
+        render json: recipe.to_json(
+            {:except => [:created_at, :updated_at]}
+        )
+    end
+    def
+
+    
 end
