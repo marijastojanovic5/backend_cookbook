6cfb03199ad470329bb872b3e600dc24c580922d
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-05-05 13:17:41 -0400

    user creations done

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,7 @@
 class IngredientsController < ApplicationController
+    def index
+        ingredient =Ingredient.all
+        render json: ingredient.to_json(
+            {:except => [:created_at, :updated_at] })
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index
+        user=User.all
+        render json: user.json()
+        
+    end
+    def show
+        user =User.find(params[:id])
+        render json: user.json()
+
+    end
+
+    def create
+        
+        user =User.find_or_create_by(first_name: params[:firstName],last_name: params[:lastName],user_name: params[:username],password: params[:password])
+        render json: {user: user}
+    end
+    
 end
