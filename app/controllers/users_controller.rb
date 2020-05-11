805cb552ba0fd5e4a2d19b8206b01f5e9ca69a71
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-05-11 13:22:11 -0400

    done with user + favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
     def index
         user=User.all
-        render json: user.json()
+        render json: user.map{ |user| {user: user, favorites: user.recipe}}
         
     end
     def show
-        user =User.find(params[:id])
-        render json: user.json()
+        user = User.find(params[:id])
+        render json: {user: user, favorites: user.recipe}
 
     end
 
     def create
         
-        user =User.find_or_create_by(first_name: params[:firstName],last_name: params[:lastName],user_name: params[:username],password: params[:password])
+        user =User.find_or_create_by(first_name: params[:firstName],last_name: params[:lastName],username: params[:username],password: params[:password])
         render json: {user: user}
     end
     
