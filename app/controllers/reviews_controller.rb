c7a70ad10ebc7e041d7222d9499fb3066c26437f
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-05-22 11:18:52 -0400

    reviews done but not persisting

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,14 @@
 class ReviewsController < ApplicationController
     def create
        
-        review = Review.create(user_id: params[:user_id], recipe_id: params[:recipe_id], rating: params[:rating],review: params["reviewText"])
-        render json: { recipe_id: review.recipe, review: review.review, id: review.id, rating: review.rating}
+        review = Review.find_or_create_by(user_id: params[:user_id], recipe_id: params[:recipe_id], rating: params[:rating],review: params[:reviewText])
+        render json: { recipe_id: review.recipe.id, review: review.review, id: review.id, rating: review.rating}
     end
     
 
     # def destroy
-    #     comment =Comment.find(params[:id])
-    #     comment.destroy
+    #     review =Review.find(params[:id])
+    #     review.destroy
     
     # end
 end
