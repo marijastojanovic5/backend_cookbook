95198a62abbb00624c6066931bf857c0c2d553bb
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-06-02 12:19:16 -0400

    went back and changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Recipe.destroy_all
 Ingredient.destroy_all
 
 api_key=Rails.application.credentials.spoonacular[:api_key]
-response_recipe =RestClient.get("https://api.spoonacular.com/recipes/random?number=20&apiKey=#{api_key}")
+response_recipe =RestClient.get("https://api.spoonacular.com/recipes/random?number=10&apiKey=#{api_key}")
 recipes = JSON.parse(response_recipe)
 recipe_array = recipes['recipes']
 
@@ -22,7 +22,6 @@ recipe_array.each do |recipe|
     Recipe.create(title: title, cook_time: cook_time, instructions: instructions, picture: picture, health_score: health_score, gluten_free: gluten_free, vegetarian: vegetarian, vegan: vegan, dairy_free: dairy_free)
   end
 
-roasted_chicken = Recipe.find_or_create_by(title: "Roasted Chicken",cook_time: 30, instructions: "Roast in the oven", picture: "picture", health_score: 20, gluten_free: true, vegetarian: false, vegan: false, dairy_free: true)
 chicken = Ingredient.find_or_create_by(name:"Chicken", amount: 1.0, unit: "whole")
 garlic = Ingredient.find_or_create_by(name:"Garlic", amount: 1.0, unit: "clove")
 sugar = Ingredient.find_or_create_by(name:"Sugar", amount: 1.0, unit: "tsp")
@@ -31,6 +30,3 @@ milk = Ingredient.find_or_create_by(name:"Whole Milk", amount: 0.5, unit: "cup")
 egg = Ingredient.find_or_create_by(name:"Egg", amount: 1.0, unit: "each")
 salt = Ingredient.find_or_create_by(name:"Kosher Salt", amount: 1.0, unit: "tsp")
 cinnamon = Ingredient.find_or_create_by(name:"Ground Cinnamon", amount: 1.0, unit: "tsp")
-RecipeIngredient.find_or_create_by(recipe_id: roasted_chicken.id , ingredient_id: sugar.id)
-RecipeIngredient.find_or_create_by(recipe_id: roasted_chicken.id , ingredient_id: garlic.id)
-RecipeIngredient.find_or_create_by(recipe_id: roasted_chicken.id , ingredient_id: chicken.id)
