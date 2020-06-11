ddbd74925dc1dff0d17ca361eb8b987e4281e623
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-06-11 12:18:19 -0400

    ingredients added into seed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,7 @@ class RecipesController < ApplicationController
     def show
         recipe = Recipe.find(params[:id])
         render json: recipe.to_json(
-            {:except => [:created_at, :updated_at], :include =>[:reviews,:users]})
+            {:except => [:created_at, :updated_at], :include =>[:ingredients,:reviews,:users]})
                
 
     end
@@ -21,6 +21,12 @@ class RecipesController < ApplicationController
         favorite_rec= Favorite.find_or_create_by(user_id: params[:user_id], recipe_id: params[:recipe_id])
         render json: favorite_rec.recipe
     end
+
+    def recipe_ingredient
+        recipe_ingredient = RecipeIngredients.create(ingredient_id: params[:ingredient_id] , recipe_id: params[:recipe_id])
+        render json: recipe_ingredient.ingredient
+    end
+
     def destroy
        
         user_id=params[:user_id]
